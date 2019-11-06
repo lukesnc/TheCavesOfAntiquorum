c0bf76285b5cd1cc79bec03545879e8da2516b0a
lukesnc <35816797+lukesnc@users.noreply.github.com>
2019-11-06 13:49:27 -0500

    started story of act 3

--- TheCavesOfAntiquorum/act3.py
+++ TheCavesOfAntiquorum/act3.py
@@ -1,7 +1,7 @@
 # Act 3
 
 # KEY FEATURES
-# inventory and equipment
+# weapons and equipment
 # more lore of game explored
 # combat and enemies
 
@@ -13,9 +13,37 @@ from time import sleep
 
 # Globals
 p1 = player.Player()
-weapon = 0
 
+# Story of act 3 starts here
+def attemptRock():
+  print("it's darker than before, but the rubble is still there")
+  sleep(1)
+  print("and an explosive in your hands")
+  sleep(2)
+  print("\nyou need something to light the dynamite with")
+  sleep(2)
 
+  while True:
+    print("\nlook around?\n\nyes or no")
+    option = input("> ")
+
+    if option == "yes":
+      print("you look around on the ground, pushing over rocks and feeling around on the walls")
+      sleep(1)
+      print("you find a piece of flint")
+      # FINISH THIS
+      break
+    elif option == "no":
+      print("you decide against looking for something to light the dynamite with")
+      sleep(2)
+      printSlow("\nwhy not give youself a name?\n")
+      sleep(1)
+      p1.getName()
+      sleep(2)
+      clearScreen()
+      attemptRock()
+      break
+    inputError(option)
 
 def start():
   clearScreen()
@@ -25,4 +53,5 @@ def start():
   input()
   clearScreen()
   sleep(3)
+  attemptRock()
   
