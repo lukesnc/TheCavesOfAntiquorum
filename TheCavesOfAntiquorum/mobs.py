95c04fd9bac30e8dd02f5e8d5a78119f8c902493
lukesnc <35816797+lukesnc@users.noreply.github.com>
2019-11-07 09:32:31 -0500

    started creating enemy encounter function

--- TheCavesOfAntiquorum/mobs.py
+++ TheCavesOfAntiquorum/mobs.py
@@ -12,33 +12,80 @@ def getRandomWeapon():
     items.Club.ID: items.Club()
   }
 
-  w = randint(const.ENEMY_WEAPON_ID_MIN, const.ENEMY_WEAPON_ID_MAX)
+  r = randint(1, 10)
+  if r < 3: # 1/3 to get stone
+    w = items.Stone.ID
+  elif r > 3 and r < 10: # 3/5 to get stick
+    w = items.Stick.ID
+  else: # 1/10 to get club
+    w = items.Club.ID
+
+  # w = randint(const.ENEMY_WEAPON_ID_MIN, const.ENEMY_WEAPON_ID_MAX)
   return enemyWeapon[w]
 
-# ENEMIES 
-    
-class Rat:
-  name = "rat"
+# Returns a random minor enemy
+def getRandomEnemy():
+  # Dictionary of enemies
+  enemies = {
+    Rat.name: Rat(),
+    Spider.name: Spider(),
+    UndeadSoldier.name: UndeadSoldier()
+  }
+
+  r = randint(1, 10)
+  if r < 3: # 1/3 to get rat
+    e = Rat.name
+  elif r > 3 and r < 10: # 3/5 to get spider
+    e = Spider.name
+  else: # 1/10 to get undead soldier
+    e = UndeadSoldier.name
+
+  return enemies[e]
   
-  def __init__(self):
-    self.health = 5
+# Random enemy encounter, pass in class player
+def encounterEnemy(player):
+  e = getRandomEnemy()
 
-class Spider:
-  name = "spider"
+# ENEMIES 
 
-  def __init__(self):
-    self.health = 10
+# Main enemy class (ALL ENEMIES SHARE THESE FUNCTIONS AND ATTRIBUTES)
+class Enemy(object):
+  # Variables that enemies have
+  name = None
+  damage = None
+  weapon = None
+  health = None
 
-class UndeadSoldier:
-  name = "undead soldier"
-
-  def __init__(self):
-    self.health = 15
-    self.weapon = getRandomWeapon()
-    
+  # Combat functions
   def attack(self):
     return self.weapon.damage
 
   def takeDamage(self, damage):
     self.health = self.health - damage
+
+
+# Sub-classes
+    
+class Rat(Enemy):
+  name = "rat"
+  damage = 2
+  
+  def __init__(self):
+    self.health = 5
+
+class Spider(Enemy):
+  name = "spider"
+  damage = 4
+
+  def __init__(self):
+    self.health = 10
+
+class UndeadSoldier(Enemy):
+  name = "undead soldier"
+
+  def __init__(self):
+    self.health = 15
+    self.weapon = getRandomWeapon()
+    self.damage = self.weapon.damage
+    
 
