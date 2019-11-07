1953c675b333a168a66474aac23c54260c46d412
lukesnc <35816797+lukesnc@users.noreply.github.com>
2019-11-07 09:31:29 -0500

    added consts for enemy encounter

--- TheCavesOfAntiquorum/const.py
+++ TheCavesOfAntiquorum/const.py
@@ -9,5 +9,9 @@ PLAYER_OBJ_PATH = "TheCavesOfAntiquorum/save/player.obj"
 PLAYER_OBJ_BACKUP_PATH = "TheCavesOfAntiquorum/save/player.obj.bak"
 
 # Used for rolling a random enemy weapon to hold
-ENEMY_WEAPON_ID_MIN = 61
-ENEMY_WEAPON_ID_MAX = 63
+# ENEMY_WEAPON_ID_MIN = 61
+# ENEMY_WEAPON_ID_MAX = 63
+
+# Returns from combat encounter
+DEAD = 0
+ALIVE = 1
