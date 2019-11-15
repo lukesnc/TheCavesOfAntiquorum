a7da98bc4357ee4e1740d60cd3fd9d524bb078f1
lukesnc <35816797+lukesnc@users.noreply.github.com>
2019-11-15 13:05:38 -0500

    cleaned up consts

--- TheCavesOfAntiquorum/const.py
+++ TheCavesOfAntiquorum/const.py
@@ -1,22 +1,17 @@
 # File containing useful constants
 
+# FILE PATHS
+
 # Path to save file from root folder
 SAVE_PATH = "TheCavesOfAntiquorum/save/save.txt"
 SAVE_BACKUP_PATH = "TheCavesOfAntiquorum/save/save.txt.bak"
-
 # Path to file containing obj of player from root folder
 PLAYER_OBJ_PATH = "TheCavesOfAntiquorum/save/player.obj"
 PLAYER_OBJ_BACKUP_PATH = "TheCavesOfAntiquorum/save/player.obj.bak"
 
-# Chance to encounter enemy at certain points
-ENCOUNTER_CHANCE = 1/3
-
-# Returns from combat encounter
-DEAD = 0
-ALIVE = 1
-
-# Armor damage change
-ARMOR_MOD = 0.5
-
-# Enemy is unkillable
-UNKILLABLE = 999
+# COMBAT AND PLAYER CONSTANTS
+ENCOUNTER_CHANCE = 1/3 # Chance to encounter enemy at certain points
+DEAD = 0 # Returns from combat encounter
+ALIVE = 1 # Same as above
+ARMOR_MOD = 0.5 # Armor damage modifier
+UNKILLABLE = 999 # Health value given to an unkillable enemy
