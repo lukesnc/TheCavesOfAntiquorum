2d496f3c38513f221c291b858954e7778c9959c8
lukesnc <35816797+lukesnc@users.noreply.github.com>
2019-12-05 12:47:51 -0500

    seperated combat and mobs

--- TheCavesOfAntiquorum/act3.py
+++ TheCavesOfAntiquorum/act3.py
@@ -9,7 +9,7 @@
 from TheCavesOfAntiquorum import items, const
 from TheCavesOfAntiquorum.player import Player
 from TheCavesOfAntiquorum.helpers import clearScreen, inputError, printSlow, printVerySlow, loadPlayer
-from TheCavesOfAntiquorum.mobs import testEnemyEncounter
+from TheCavesOfAntiquorum.combat import CombatSystem
 
 from time import sleep
 
@@ -19,6 +19,7 @@ try:
   p1 = loadPlayer()
 except:
   p1 = Player()
+fight = CombatSystem()
 
 
 # Room after rubble is blown, containing lore of game and shitty dagger
@@ -95,4 +96,7 @@ def start():
   input()
   clearScreen()
   sleep(3)
-  attemptRock()
+  # attemptRock()
+
+  # testing
+  fight.testEnemyEncounter(p1, const.ENCOUNTER_RATE)
--- TheCavesOfAntiquorum/mobs.py
+++ TheCavesOfAntiquorum/mobs.py
@@ -1,183 +1,17 @@
 # File for enemy database and combat system
 
-from TheCavesOfAntiquorum import items, const
-from TheCavesOfAntiquorum.helpers import clearScreen, printSlow, inputError
+from TheCavesOfAntiquorum import items
+from TheCavesOfAntiquorum.helpers import printSlow
 
-from random import randint, random
+from random import random
 from time import sleep
 
-# Returns a random weapon for newly spawned enemy to hold
-# Must pass in list of allowed weapons
-def getRandomWeapon(allowed):
-  # Dictionary of enemy weapons
-  enemyWeapons = {
-    items.Stick: items.Stick(),
-    items.Stone: items.Stone(),
-    items.Club: items.Club(),
-    items.Dagger: items.Dagger()
-  }
-
-  weaponReturned = False
-
-  # Checks if the enemy is allowed to use the weapon, then rolls chance to recieve it
-  while weaponReturned == False:
-    for weapon in enemyWeapons:
-      if ((weapon in allowed) and (random() < weapon.chanceToGet)):
-        weaponReturned = True
-        return enemyWeapons[weapon]
-
-
-# Returns a random minor enemy
-def getRandomEnemy():
-  # Dictionary of enemies
-  enemies = {
-    Rat: Rat(),
-    Spider: Spider(),
-    UndeadSoldier: UndeadSoldier(),
-    Goblin: Goblin(),
-    Spirit: Spirit()
-  }
-
-  enemyReturned = False
-
-  # Rolls chance to encounter wild enemies
-  while enemyReturned == False:
-    for enemy in enemies:
-      if (random() < enemy.chanceToEncounter):
-        enemyReturned = True
-        return enemies[enemy]
-
-
-# Set back to random encounter chance
-def testEnemyEncounter(player, rate):
-  if random() < rate:
-    encounterEnemy(player)
-
-# Random enemy encounter, pass in class player
-def encounterEnemy(player):
-  # Enemy and player
-  e = getRandomEnemy()
-  p = player
-
-  # Actions player can pick from, enemy will roll chance
-  playerActions = ["attack", "cower", "run"]
-  enemyActions = ["attack", "taunt"]
-
-  # Keeps track of if combat is over and whose turn it is
-  # Player plays on odd turns, enemy on even
-  combatFinished = False
-  turn = 1
-
-  # Determines if player can escape enemy (use run option)
-  def canEscape():
-    escapeChance = 0.13 * (p.MAX_HEALTH / e.health)
-    if random() < escapeChance:
-      return True
-    else:
-      return False
-
-  # Determine if anyone is dead
-  def checkHealths(pH, eH):
-    if pH < 1 or eH < 1:
-      return True
-    else:
-      return False
-
-  # Dialogue for encounter start
-  clearScreen()
-  try:
-    printSlow("you have encountered a wild " + e.name + " wielding " + e.weapon.name + "\n")
-  except: # Enemy isn't capable of weapon (rat, spider, etc...)
-    printSlow("you have encountered a wild " + e.name + "\n")
-  sleep(1)
-  printSlow("what will you do?\n\n")
-  sleep(.5)
-
-  # While the enemy and player are alive
-  while not combatFinished:
-    if turn % 2 == 1: # if turn is odd (player action)
-      # Update displayed values
-      print("\n-=HEALTH=-")
-      print("player: " + str(p.health))
-      print(e.name + ": " + str(e.health) + "\n")
-
-      while True:
-        # Print the players options and get input
-        print(playerActions[0] + ",", playerActions[1] + " or", playerActions[2] + "?")
-        action = input("> ")
-        # Act based upon input
-        if action.lower() == playerActions[0]:
-          # Takes away enemy health based on players damage
-          sleep(1)
-          e.takeDamage(p.attack())
-          # Check if someone is dead
-          combatFinished = checkHealths(p.health, e.health)
-          break
-        elif action.lower() == playerActions[1]:
-          sleep(2)
-          print("you curl up in fear")
-          sleep(1)
-          print("the enemies gaze burning a hole through you\n")
-          sleep(3)
-          printSlow("you freeze\n")
-          sleep(3)
-          print("fear has taken hold of you\n")
-          sleep(5)
-          print("you snap out of it")
-          sleep(2)
-          print("but you feel something has changed")
-          sleep(2)
-          print("quite possibly in your favor\n")
-          sleep(3)
-          # Player's chance of critical hit increases
-          p.critChance += 0.01
-          break
-        elif action.lower() == playerActions[2]:
-          if canEscape() == True:
-            printSlow("you managed to escape...\n\n")
-            combatFinished = True
-          else:
-            printSlow("you couldn't get away\n\n")
-          break
-        inputError(action)
-    
-    if turn % 2 == 0: # if turn is even (enemy action)
-      # Roll random to determine what enemy will do
-      if random() < Enemy.ATTACK_CHANCE: 
-        action = enemyActions[0]
-      else:
-        action = enemyActions[1]
-
-      sleep(1)
-      if action == enemyActions[0]: # Enemy attacks
-        # Take away player's health based on enemy damage
-        p.takeDamage(e.attack())
-        # Check if someone is dead
-        combatFinished = checkHealths(p.health, e.health)
-      else:  # Enemy taunts
-        print("the " + e.name + " is staring at you")
-        sleep(1)
-        print("it's mocking you")
-        sleep(1)
-        print("\nthe " + e.name + " says, ", end='')
-        sleep(1)
-        printSlow("\"" + e.tauntMsg + "\"\n\n")
-        sleep(1)
-
-    sleep(1)
-    turn += 1
-        
-  p.critChance = p.DEFAULT_CRIT_CHANCE
-  print("combat done")
-
-      
 # ENEMIES 
 
 # Main enemy class (ALL ENEMIES SHARE THESE FUNCTIONS AND ATTRIBUTES)
 class Enemy(object):
   # Consts
   UNKILLABLE = 999 # Health value given to an unkillable enemy
-  ATTACK_CHANCE = 3/4 # Chance enemy's random combat action will be "attack"
 
   # Variables that enemies have
   name = ""
@@ -187,6 +21,26 @@ class Enemy(object):
   tauntMsg = ""
   allowedWeapons = []
   chanceToEncounter = 1/10
+
+  # Returns a random weapon for newly spawned enemy to hold
+  # Must pass in list of allowed weapons
+  def getRandomWeapon(self):
+    # Dictionary of enemy weapons
+    enemyWeapons = {
+      items.Stick: items.Stick(),
+      items.Stone: items.Stone(),
+      items.Club: items.Club(),
+      items.Dagger: items.Dagger()
+    }
+
+    weaponReturned = False
+
+    # Checks if the enemy is allowed to use the weapon, then rolls chance to recieve it
+    while weaponReturned == False:
+      for weapon in enemyWeapons:
+        if ((weapon in self.allowedWeapons) and (random() < weapon.chanceToGet)):
+          weaponReturned = True
+          return enemyWeapons[weapon]
 
   # Combat functions
   def attack(self):
@@ -234,7 +88,7 @@ class UndeadSoldier(Enemy):
   def __init__(self):
     super().__init__()
     self.health = 15
-    self.weapon = getRandomWeapon(self.allowedWeapons)
+    self.weapon = self.getRandomWeapon()
     self.damage = self.weapon.damage
     
 class Goblin(Enemy):
@@ -249,7 +103,7 @@ class Goblin(Enemy):
   def __init__(self):
     super().__init__()
     self.health = 8
-    self.weapon = getRandomWeapon(self.allowedWeapons)
+    self.weapon = self.getRandomWeapon()
     self.damage = self.weapon.damage
 
 class Spirit(Enemy):
